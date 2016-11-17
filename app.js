8ec78613b5f2fe9762511304ecfe3cc946aafac7
0peron <phubbard89@gmail.com>
2016-11-16 19:43:36 -0500

    the drills

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("Hello console")
+
+function alertHelloWorld(argument) {
+	alert('hello world')
+}
+
+alertHelloWorld();
+
+function hello(name) {
+	return "hello" + name + "!";
+}
+
+console.log(hello('Thomas'));
+
+function averageOfTwo(num1, num2) { 
+	return (num1 + num2) / 2; 
+}
+
+averageOfTwo(10,16)
---
+++ jsdrills.js
@@ -0,0 +1,11 @@
+function wisePerson(wiseType, whatToSay) {
+  return 'A wise ' + wiseType + ' once said: "' + whatToSay + '".';
+}
+
+function shouter(whatToShout) {
+  return whatToShout.toUpperCase() + '!!!';
+}
+
+function textNormalizer(text) {
+  return text.toLowerCase().trim()
+}
